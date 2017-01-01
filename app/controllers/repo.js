0b29726605cf2d89ab8424f7282a2e94d0d47a34
billybonks <sebastienstettler@gmail.com>
2017-01-01 15:34:08 +0800

    clean more

--- app/controllers/repo.js
+++ app/controllers/repo.js
@@ -5,12 +5,5 @@ import service from 'ember-service/inject';
 export default Controller.extend({
   count:computed('model.length', function(){
     return this.get('model.length')
-  }),
-  actions:{
-
-    waitAFewSeconds(){
-      this.get('waitAFewSeconds').perform();
-    }
-
-  }
+  })
 });
