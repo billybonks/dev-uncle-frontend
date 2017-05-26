be7fa75b3d4b46f82d4afa2dc33f78dd7fa86d6b
billybonks <sebastienstettler@gmail.com>
2017-05-26 18:07:59 +0800

    fix slack

--- app/controllers/repo/manage/slack.js
+++ app/controllers/repo/manage/slack.js
@@ -1,7 +1,6 @@
 import Controller from 'ember-controller';
 import { task } from 'ember-concurrency';
 import service from 'ember-service/inject';
-import computed from 'ember-computed-decorators';
 
 export default Controller.extend({
   ajax: service(),
@@ -9,17 +8,6 @@ export default Controller.extend({
   saveSlack: task(function *(){
     yield this.get('model').save();
   }),
-  @computed
-  slackOrgs:{
-    get(){
-      this.store.findAll('slackOrganization').then( (orgs) => {
-        this.set('slackOrgs',orgs);
-      });
-    },
-    set(_, value){
-      return value;
-    }
-  },
   actions: {
     setSlackOrg(org){
       this.set('model.slackOrganization', org);
