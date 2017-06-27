23a41deb42c4d5832ef8fb936341c56f92219273
billybonks <sebastienstettler@gmail.com>
2017-06-27 22:56:41 +0800

    make ensure record reusable

--- app/routes/abstract-route.js
+++ app/routes/abstract-route.js
@@ -2,6 +2,16 @@ import Ember from 'ember';
 import service from 'ember-service/inject';
 export default Ember.Route.extend({
   fixedHeader: service(),
+  ensureRecord(parentModel,childModelName, target){
+    let pluralizedype = Ember.String.pluralize(childModelName);
+    if(!parentModel.get(`${pluralizedype}.length`)){
+      return this.store.query(childModelName, {repo_id: parentModel.get('id')}).then( (results) => {
+        if(target){
+          target.set(pluralizedype, results);
+        }
+      });
+    }
+  },
   actions: {
     didTransition: function() {
       this.set('fixedHeader.title', this.get('title'));
