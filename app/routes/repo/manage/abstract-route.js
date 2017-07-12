d3d332b7dbf89912e4a3d04713a513a32a04c510
billybonks <sebastienstettler@gmail.com>
2017-07-13 00:37:59 +0800

    flatten more components

--- app/routes/repo/manage/abstract-route.js
+++ app/routes/repo/manage/abstract-route.js
@@ -2,7 +2,7 @@ import AbstractRoute from "client/routes/abstract-route";
 
 export default AbstractRoute.extend({
   title: 'Settings',
-  controlsPath: 'fixed-controls/manage',
+  controlsPath: 'fixed-controls-manage',
   setupController(/*controller, model*/) {
     this.set('controlsParams', this.paramsFor('repo').repo_id);
   }
