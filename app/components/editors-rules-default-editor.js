30a98986d76c65c665f4160113cafd1e088cfdd2
billybonks <sebastienstettler@gmail.com>
2017-10-26 02:08:50 +0800

    future life

--- app/components/editors-rules-default-editor.js
+++ app/components/editors-rules-default-editor.js
@@ -1,18 +1,20 @@
-import Component from 'ember-component';
-import service from 'ember-service/inject';
+import Component from '@ember/component';
+import { service } from 'ember-decorators/service';
+import { action } from 'ember-decorators/object';
 
-export default Component.extend({
-  states: service(),
-  init(){
-    this._super(...arguments);
+export default class EditorsRulesDefaultEditor extends Component {
+  @service states;
+
+  constructor() {
+    super(...arguments);
     let label = this.get('labels').filterBy('id', this.get('rule.rules.apply_label'));
     this.set('selected', label[0]);
-  },
-  actions:{
-    setChangeLabel(label){
-      this.set('selected', label);
-      this.set('rule.hasDirtyAttributes', true);
-      this.set('rule.rules.apply_label', label.get('id'));
-    }
   }
-});
+
+  @action
+  setChangeLabel(label) {
+    this.set('selected', label);
+    this.set('rule.hasDirtyAttributes', true);
+    this.set('rule.rules.apply_label', label.get('id'));
+  }
+}
