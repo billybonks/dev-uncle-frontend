f453259fe65f6a4c3e59b55a313b36ddcafb7bfb
billybonks <sebastienstettler@gmail.com>
2018-01-03 23:17:31 +0800

    silly change

--- app/components/automation-builder.js
+++ app/components/automation-builder.js
@@ -1,57 +1,75 @@
 import Component from '@ember/component';
 import PullRequest from 'client/models/pull-request';
 
-const actions = [
-  {
-    label: 'Apply Label',
-    key: 'apply_label',
-    editor: 'association'
-  },
-  {
-    label: 'Message Reviewers',
-    key: 'message_reviewers'
-  },
-  {
-    label: 'Message Owner',
-    key: 'message_owner'
-  },
-  {
-    label: 'Message Channel',
-    key: 'message_channel',
-    editor: 'text'
-  }
-];
+const applyLabel =  {
+  label: 'Apply Label',
+  key: 'apply_label',
+  editor: 'association'
+};
+
+const messageReviewers = {
+  label: 'Message Reviewers',
+  key: 'message_reviewers'
+  };
+
+const messageOwner = {
+  label: 'Message Owner',
+  key: 'message_owner'
+};
+
+const messageChannel= {
+  label: 'Message Channel',
+  key: 'message_channel',
+  editor: 'text'
+};
+
+const messageLastCommitter = {
+  label: 'Message Last Committer',
+  key: 'message_last_commit'
+};
+
+const actions = [applyLabel,messageReviewers,messageOwner,messageChannel];
 
 export default class AutomationBuilder extends Component {
-  events = [
-    {
-      name:'pull_request_synchronize',
-      displayName: 'PR Code Updated',
-      actions: actions
-    },
-    {
-      name:'pull_request_rejected',
-      displayName: 'Reviewer Requested Changes',
-      actions: actions
-    },
-    {
-      name:'pull_request_approved',
-      displayName: 'Reviewer Approved Changes',
-      actions: actions
-    },
-    {
-      name:'pull_request_opened',
-      displayName: 'PR Opened',
-      actions: actions
-    }
-  ];
 
   constructor(){
     super(...arguments);
+    this.constructEvents();
     let prTypes = this.generateTypeArray(PullRequest.prototype);
     this.get('events').forEach( (event) => {
       event.attributes = prTypes;
     });
+  }
+
+  constructEvents(){
+    let events = [
+      {
+        name:'pull_request_synchronize',
+        displayName: 'PR Code Updated',
+        actions: actions
+      },
+      {
+        name:'branch_built',
+        displayName: 'Branch CI has run',
+        actions: [messageChannel, messageLastCommitter],
+      },
+      {
+        name:'pull_request_rejected',
+        displayName: 'Reviewer Requested Changes',
+        actions: actions
+      },
+      {
+        name:'pull_request_approved',
+        displayName: 'Reviewer Approved Changes',
+        actions: actions
+      },
+      {
+        name:'pull_request_opened',
+        displayName: 'PR Opened',
+        actions: actions
+      }
+    ];
+    this.set('events', events);
   }
 
   generateTypeArray(KlassProto){
