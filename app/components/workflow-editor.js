128f6e15c6931c1b3e19eb7806d05ce3fb3ddd97
billybonks <sebastienstettler@gmail.com>
2018-01-06 15:46:09 +0800

    workflow-editor component

---
+++ app/components/workflow-editor.js
@@ -0,0 +1,27 @@
+import Component from '@ember/component';
+import RSVP from 'rsvp';
+import {action} from 'ember-decorators/object';
+import { service } from 'ember-decorators/service';
+export default class ComponentWorkflowEditor extends Component {
+  @service store;
+
+  @action
+  addRule(){
+    let rule = this.get('store').createRecord('rule');
+    rule.set('repo', this.get('workflow.repo') );
+    this.get('workflow.rules').pushObject(rule);
+  }
+
+  @action
+  saveWorkflow(){
+    let promises = this.get('workflow.rules').map( (rule) => {
+      if(rule.get('hasDirtyAttributes')){
+        rule.set('hasDirtyAttributes', false);
+        return rule.save();
+      }
+    });
+    RSVP.all(promises).then(() => {
+      this.get('workflow').save();
+    });
+  }
+}
