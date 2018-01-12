14d613819ca33461e895417c0260c386c2b15b99
billybonks <sebastienstettler@gmail.com>
2018-01-12 20:28:57 +0800

    temp migration path

--- app/routes/repo/workflows/new.js
+++ app/routes/repo/workflows/new.js
@@ -4,6 +4,7 @@ export default class RouteWorkflowNew extends AbstractRoute {
   async model(/*params*/){
     let repo = await this.store.findRecord('repo', this.paramsFor('repo').repo_id);
     let workflow = this.store.createRecord('workflow');
+    workflow.set('rules', repo.get('rules'));
     workflow.set('repo', repo);
     return {
       repo, workflow
