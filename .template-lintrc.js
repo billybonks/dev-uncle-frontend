e47a9ba0513602e1da69e23c97aba1eb8f58da8f
billybonks <sebastienstettler@gmail.com>
2018-07-21 15:51:53 +0800

    more rules

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -5,6 +5,12 @@ module.exports = {
   extends: 'recommended',
 
   rules: {
-    'no-bare-strings': false
+    'no-bare-strings': false,
+    'no-implicit-this': false,
+    'eol-last': 'always',
+    'attribute-indentation': false,
+    'inline-link-to': true,
+    'no-inline-styles' :true,
+    'no-duplicate-attributes': true
   }
 };
