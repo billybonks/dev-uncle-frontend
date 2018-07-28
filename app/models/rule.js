ecaee023e721049964705749e9180d455220584b
billybonks <sebastienstettler@gmail.com>
2018-07-28 21:03:10 +0800

    add conditions support

--- app/models/rule.js
+++ app/models/rule.js
@@ -1,9 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  rules:     DS.attr(),
-  event:     DS.attr('string'),
-  createdAt: DS.attr('date'),
-  updatedAt: DS.attr('date'),
-  repo:      DS.belongsTo('repo'),
+  rules:      DS.attr(),
+  conditions: DS.attr({defaultValue() { return [{}];}}),
+  event:      DS.attr('string'),
+  createdAt:  DS.attr('date'),
+  updatedAt:  DS.attr('date'),
+  repo:       DS.belongsTo('repo'),
 });
