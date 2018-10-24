0fa83554b49715df2cff2409627d5418aefa57d2
billybonks <sebastienstettler@gmail.com>
2018-10-24 21:38:11 +0800

    fix endentation

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -8,7 +8,9 @@ module.exports = {
     'no-bare-strings': false,
     'no-implicit-this': true,
     'eol-last': 'always',
-    'attribute-indentation': false,
+    'attribute-indentation': {
+      'open-invocation-max-len': 110,
+    },
     'inline-link-to': true,
   }
 };
