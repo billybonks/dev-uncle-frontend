6bb40e76641f77ed7b1f63012f131193919df0c6
billybonks <sebastienstettler@gmail.com>
2018-11-06 21:45:27 +0800

    remove slack channel from front end

--- app/models/slack-setting.js
+++ app/models/slack-setting.js
@@ -1,7 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  slackChannel: DS.attr('string'),
   createdAt: DS.attr('date'),
   updatedAt: DS.attr('date'),
   slackOrganization: DS.belongsTo('slackOrganization'),
