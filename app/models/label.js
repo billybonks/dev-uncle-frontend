50bbbc241d7e977afc02e0b09ad88aa72edfd78f
billybonks <sebastienstettler@gmail.com>
2018-11-10 23:20:24 +0800

    more labels

--- app/models/label.js
+++ app/models/label.js
@@ -1,7 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  isState:   DS.attr('boolean'),
   name:      DS.attr('string'),
   online_id: DS.attr('number'),
   color:     DS.attr('string'),
