c2bd4aae4eabd867137baa0b2dd8c02beb1675a7
billybonks <sebastienstettler@gmail.com>
2018-11-13 11:18:42 +0800

    add initial user store service

---
+++ app/services/users.js
@@ -0,0 +1,41 @@
+import Service from '@ember/service';
+import Repo from 'client/models/repo';
+import Organisation from 'client/models/organisation';
+import { inject } from '@ember/service';
+
+export default Service.extend({
+  store: inject(),
+  init(){
+    this._super(...arguments);
+    this.repoUsers = new Map();
+    this.orgUsers = new Map();
+  },
+  query(params){
+    return this.store.query('user', params);
+  },
+  async findUsers(repo){
+    let repoId = repo.get('id');
+    let organisationId = repo.get('organisation.id');
+    let repoUsers = await this.findRepoMembers(repoId);
+    let orgUsers = await this.findOrganisationMembers(organisationId);
+    return Array.from(new Set([...repoUsers, ...orgUsers]));
+  },
+  async findRepoMembers(repoId){
+    if(this.repoUsers.has(repoId)){
+       return this.repoUsers.get(repoId);
+    }
+    let users = await this.query({
+      repoId: repoId
+    });
+    return users.toArray();
+  },
+  async findOrganisationMembers(organisationId){
+    if(this.orgUsers.has(organisationId)){
+       return this.orgUsers.get(organisationId);
+    }
+    let users = await this.query({
+      organisationId: organisationId
+    });
+    return users.toArray();
+  }
+});
