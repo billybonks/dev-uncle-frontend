eb1779b8bf1b237b6316c6a85d757548d73a5eff
billybonks <sebastienstettler@gmail.com>
2019-01-27 14:29:28 +0800

    add repo selection

--- app/routes/repo/workflows/edit.js
+++ app/routes/repo/workflows/edit.js
@@ -4,6 +4,8 @@ export default Route.extend({
   async setupController(controller){
     this._super(...arguments);
     let repo = await this.store.findRecord('repo', this.paramsFor('repo').repo_id);
+    let organisation = await this.store.findRecord('organisation', repo.get('organisation.id'));
     controller.set('repo', repo);
+    controller.set('organisation', organisation);
   }
 });
