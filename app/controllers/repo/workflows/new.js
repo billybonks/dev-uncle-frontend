0a23640d2b9d5eca0664d5bda49c04c434388fd9
billybonks <sebastienstettler@gmail.com>
2019-03-24 13:34:22 +0800

    [REFACTOR] Route big fan of just returning one item from model

--- app/controllers/repo/workflows/new.js
+++ app/controllers/repo/workflows/new.js
@@ -1,11 +1,7 @@
 import Controller from '@ember/controller';
-import { alias } from '@ember/object/computed';
 import { action } from '@ember-decorators/object';
 
 export default class ControllerWorkflowsNew extends Controller {
-  repo = alias('model.repo')
-  workflow = alias('model.workflow')
-
   @action
   transitionAfterSave() {
     this.transitionToRoute('repo.manage.workflows', this.get('repo'));
--- app/routes/repo/workflows/new.js
+++ app/routes/repo/workflows/new.js
@@ -2,11 +2,15 @@ import AbstractRoute from 'client/routes/repo/manage/abstract-route';
 
 export default class RouteWorkflowNew extends AbstractRoute {
   async model() {
-    const repo = await this.store.findRecord('repo', this.paramsFor('repo').repo_id);
+    this.repo = await this.store.findRecord('repo', this.paramsFor('repo').repo_id);
     const workflow = this.store.createRecord('workflow');
-    workflow.set('repo', repo);
-    return {
-      repo, workflow,
-    };
+    workflow.set('repo', this.repo);
+    return workflow;
+  }
+
+  async setupController(controller, model) {
+    super.setupController(controller, model);
+    controller.set('model', model);
+    controller.set('repo', this.repo);
   }
 }
