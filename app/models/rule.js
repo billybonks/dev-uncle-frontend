4f52967f9a6857fb6e75cd9c4b6b605bfe302ed1
billybonks <sebastienstettler@gmail.com>
2019-03-25 22:45:26 +0800

    simplify code a bunch more by moving logic to model

--- app/models/rule.js
+++ app/models/rule.js
@@ -1,13 +1,26 @@
 import DS from 'ember-data';
 import { attr, belongsTo } from '@ember-decorators/data';
+import { service } from '@ember-decorators/service';
+import { computed } from '@ember-decorators/object';
 
 const { Model } = DS;
 
 export default class Rule extends Model {
+  @service automation;
   @attr('string') event;
   @attr('date') createdAt;
   @attr('date') updatedAt;
   @attr() rules;
   @attr('number', { defaultValue() { return [{}]; } }) conditions;
   @belongsTo('repo') repo;
+
+  @computed('event')
+  get eventSchema() {
+    return this.automation.eventHash[this.get('event')];
+  }
+
+  @computed('rules.action', 'eventSchema')
+  get actionSchema() {
+    return this.automation.actions.find(action => action.key === this.get('rules.action'));
+  }
 }
